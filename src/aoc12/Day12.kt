b4ca4947619d34f00015436d10870de4f6562268
JaapBeetstra <jaap@app-it-up.com>
2023-12-12 10:39:52 +0100

    Changed Day 12 to explicitly use the remaining number of items in the current group

--- src/aoc12/Day12.kt
+++ src/aoc12/Day12.kt
@@ -7,38 +7,46 @@ private const val SPR_UNKNOWN = '?'
 private const val SPR_BROKEN = '#'
 private const val SPR_WORKING = '.'
 
-data class SpringConfig(val s: String, val l: List<Int>) {
-    private val knownValues = mutableMapOf<Pair<Int, Int>, Long>()
-    private fun possibilities(idxS: Int, idxL: Int): Long =
-        knownValues[idxS to idxL] ?: calculatePossibilities(idxS, idxL).also {
-            knownValues[idxS to idxL] = it
+data class SpringConfig(val s: String, val groups: List<Int>) {
+    private val knownValues = mutableMapOf<Triple<Int, Int, Int>, Long>()
+    private fun possibilities(idxS: Int, idxL: Int, groupRemainder: Int): Long =
+        knownValues[Triple(idxS, idxL, groupRemainder)] ?: calculatePossibilities(idxS, idxL, groupRemainder).also {
+            knownValues[Triple(idxS, idxL, groupRemainder)] = it
         }
 
-    private fun calculatePossibilities(idxS: Int, idxL: Int): Long {
-        if (idxS >= s.length) return if (idxL == l.size) 1 else 0
-        if (idxL == l.size) return if (s.lastIndexOf(SPR_BROKEN) < idxS) 1 else 0
+    private fun calculatePossibilities(idxS: Int, idxGroup: Int, groupRemainder: Int): Long {
+        if (idxS >= s.length) return if (groupRemainder <= 0 && idxGroup == groups.size) 1 else 0
         return when (val next = s[idxS]) {
-            SPR_WORKING -> possibilities(idxS + 1, idxL)
-            SPR_BROKEN -> calculateWithGroup(idxL, idxS)
-            SPR_UNKNOWN -> calculateWithGroup(idxL, idxS) + possibilities(idxS + 1, idxL)
-            else -> error("Unexpected character: $next")
+            SPR_WORKING -> if (groupRemainder <= 0) noActiveGroup(idxS, idxGroup) else 0
+            SPR_BROKEN -> when {
+                groupRemainder < 0 -> startNewGroup(idxS, idxGroup)
+                groupRemainder == 0 -> 0
+                else -> continueCurrentGroup(idxS, idxGroup, groupRemainder)
+            }
+
+            SPR_UNKNOWN -> when {
+                groupRemainder < 0 -> startNewGroup(idxS, idxGroup) + noActiveGroup(idxS, idxGroup)
+                groupRemainder == 0 -> noActiveGroup(idxS, idxGroup)
+                else -> continueCurrentGroup(idxS, idxGroup, groupRemainder)
+            }
+
+            else -> {
+                error("Unexpected character: $next")
+            }
         }
     }
 
-    private fun calculateWithGroup(idxL: Int, idxS: Int): Long {
-        val groupSize = l[idxL]
-        val groupOption = s.drop(idxS).take(groupSize)
-        if (groupOption.length < groupSize || groupOption.any { it == SPR_WORKING }) return 0
-        val nextIdxS = idxS + groupSize
-        return when {
-            nextIdxS == s.length -> if (idxL + 1 == l.size) 1 else 0
-            s[nextIdxS] == SPR_BROKEN -> 0
-            else -> possibilities(nextIdxS + 1, idxL + 1)
-        }
-    }
+    private fun noActiveGroup(idxS: Int, idxGroup: Int) =
+        possibilities(idxS + 1, idxGroup, -1)
 
-    fun part1() = possibilities(0, 0)
-    fun unfold() = SpringConfig("$s?".repeat(5).dropLast(1), List(5) { l }.flatten())
+    private fun startNewGroup(idxS: Int, idxGroup: Int) =
+        if (idxGroup >= groups.size) 0 else possibilities(idxS + 1, idxGroup + 1, groups[idxGroup] - 1)
+
+    private fun continueCurrentGroup(idxS: Int, idxGroup: Int, groupRemainder: Int) =
+        possibilities(idxS + 1, idxGroup, groupRemainder - 1)
+
+    fun part1() = possibilities(0, 0, -1)
+    fun unfold() = SpringConfig("$s?".repeat(5).dropLast(1), List(5) { groups }.flatten())
 }
 
 fun main() {
@@ -64,14 +72,12 @@ fun main() {
     fun part1(input: List<String>): Long =
         input.map { s -> SpringConfig(s) }.sumOf { it.part1() }
 
-    fun part2(input: List<String>): Long = input.map { s -> SpringConfig(s) }.map { it.unfold() }.sumOf {
-        it.part1()
-    }
+    fun part2(input: List<String>): Long =
+        input.map { s -> SpringConfig(s) }.map { it.unfold() }.sumOf { it.part1() }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc12/Day12_test")
     check(part1(testInput) == 21L)
-
 
     val input = readInput("aoc12/Day12")
     part1(input).println()
