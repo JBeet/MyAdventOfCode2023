f3b1d87ece8895531fc843ca59f59ca1dfacbdf0
JaapBeetstra <jaap@app-it-up.com>
2023-12-15 09:24:54 +0100

    Cleanup day 15

--- src/aoc15/Day15.kt
+++ src/aoc15/Day15.kt
@@ -5,10 +5,12 @@ import utils.println
 import utils.readInput
 
 fun main() {
+    fun String.hash() = this.fold(0) { acc, ch -> ((acc + ch.code) * 17) % 256 }
+
     fun part1(input: List<String>) = input[0].split(',').sumOf { it.hash() }
 
-    class Boxes() {
-        val items = LinkedHashMap<Long, LinkedHashMap<String, Int>>()
+    class Boxes {
+        private val items = List(256) { LinkedHashMap<String, Int>() }
         fun process(cmd: String) {
             if (cmd.endsWith('-'))
                 processDash(cmd.dropLast(1))
@@ -16,28 +18,23 @@ fun main() {
                 cmd.split('=').let { processEquals(it[0], it[1].toInt()) }
         }
 
-        private fun processEquals(label: String, length: Int) = box(label).put(label, length)
         private fun processDash(label: String) = box(label).remove(label)
-        private fun box(label: String): LinkedHashMap<String, Int> =
-            items.getOrPut(label.hash()) { LinkedHashMap() }
-
-        fun calculate() = items.map { (nr, box) ->
-            box.asIterable().mapIndexed { slot, (nm, length) -> (nr + 1) * (slot + 1) * length }.sum()
+        private fun processEquals(label: String, length: Int) = box(label).put(label, length)
+        private fun box(label: String) = items[label.hash()]
+        fun calculate() = items.mapIndexed { nr, box ->
+            box.toList().mapIndexed { slot, (_, length) -> (nr + 1) * (slot + 1) * length }.sum()
         }.sum()
     }
 
-
-    fun part2(input: List<String>) = Boxes().apply { input[0].split(',').forEach { cmd -> process(cmd) } }.calculate()
+    fun part2(input: List<String>) = Boxes().apply { input[0].split(',').forEach(::process) }.calculate()
 
     assertEquals(200, "H".hash())
     assertEquals(52, "HASH".hash())
 
     val testInput = readInput("aoc15/Day15_test")
-    check(part1(testInput) == 1320L)
+    assertEquals(part1(testInput), 1320)
     val input = readInput("aoc15/Day15")
     part1(input).println()
-    check(part2(testInput) == 145L)
+    assertEquals(part2(testInput), 145)
     part2(input).println()
 }
-
-private fun String.hash(): Long = this.fold(0) { acc, ch -> ((acc + ch.code) * 17) % 256 }
